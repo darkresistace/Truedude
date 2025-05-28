aca7b4f2d8af3bac377303399222f3a944ef41ba
darkresistace <101935302+darkresistace@users.noreply.github.com>
2025-05-28 17:36:28 +0530

    Add files via upload

---
+++ Nilesh1.py
@@ -0,0 +1,9 @@
+#A Program to Add Subtract Multiply and Divide two Numbers
+a=input('Enter your First No: ')
+b=input('Enter your Second No: ')
+a=int(a)
+b=int(b)
+print('Addition: =',a+b)
+print('Subtraction: =',a-b)
+print('Multiplication: =',a*b)
+print('Division: =',a/b)
