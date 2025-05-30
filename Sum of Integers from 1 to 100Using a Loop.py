0fdd6b49943973b7f345d5195bd5599068a3f41d
darkresistace <101935302+darkresistace@users.noreply.github.com>
2025-05-31 02:23:31 +0530

    Update Sum of Integers from 1 to 100Using a Loop.py

--- Sum of Integers from 1 to 100Using a Loop.py
+++ Sum of Integers from 1 to 100Using a Loop.py
@@ -1,8 +1,8 @@
 total = 0
 
 # Loop from 1 to 100
-for i in range(1, 101):
+for i in range(1, 51):
     total += i
 
 # Print the result
-print("The sum of integers from 1 to 100 is:", total)
+print("The sum of integers from 1 to 50 is:", total)
